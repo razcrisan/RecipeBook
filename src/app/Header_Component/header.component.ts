580e951d856773e5087ee6388b1fba80ef8433b1
Razvan Crisan <rcrisan13@gmail.com>
2018-02-01 12:55:16 -0500

    splitting comp

--- src/app/Header_Component/header.component.ts
+++ src/app/Header_Component/header.component.ts
@@ -1,4 +1,4 @@
-﻿import { Component } from '@angular/core';
+import { Component, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +6,10 @@
 })
 
 export class HeaderComponent {
+  featureSelected = new EventEmitter<string>();
 
+  onSelect(feature: string) {
+    this.featureSelected.emit(feature);
+
+  }
 }
